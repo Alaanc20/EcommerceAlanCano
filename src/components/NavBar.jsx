f32b84b3b346862a3989d3b5b38db72ac5dde77c
Alaanc20 <alaanc20@gmail.com>
2023-05-24 19:52:12 -0300

    AlanCanoPrimerPreEntrega

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,13 +3,16 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Image from 'react-bootstrap/Image'; 
 import CartWidget from './CartWidget';
 
 const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Home</Navbar.Brand>
+      <Navbar.Brand href="#home">
+          <img src="/Lebron.png" alt="Logo" style={{ width: '100px', height: 'auto' }} />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
